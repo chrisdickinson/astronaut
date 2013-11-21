99019c8315d36977c15662578b375636420c8719
Chris Dickinson <christopher.s.dickinson@gmail.com>
2013-11-20 20:29:06 -0800

    stub stub stub

---
+++ index.js
@@ -0,0 +1,9 @@
+var workshopper = require('workshopper')
+var path = require('path')
+
+workshopper({
+    name: 'astronaut'
+  , title: 'MANIPULATE THE AST TO GET SUPERPOWERS'
+  , appDir: __dirname
+  , helpFile: path.join(__dirname, 'help.txt')
+})
